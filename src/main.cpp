9250c17428599615cc1fccb8f3c7cb3db80b5d9a
ScarletDragonFox <scarletdragonfox@gmail.com>
2025-03-29 22:42:33 +0100

    Added new dependancy: bullet3.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <Labyrinth/test_header.hpp>
+
 #include <GLFW/glfw3.h>
 #include <assimp/Importer.hpp>
 #include <glm/glm.hpp>
 #include <stb_image.h>
+
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 #include <imgui.h>
+
 #include <nlohmann/json.hpp>
+#include <bullet/btBulletCollisionCommon.h>
 
 int main()
 {
