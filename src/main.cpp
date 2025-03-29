9961d38a8e480ef87e1733dbb30d0731db515df6
ScarletDragonFox <scarletdragonfox@gmail.com>
2025-03-30 00:38:49 +0100

    Add SoLoud dependancy. Modify main.cpp to test sound.

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,44 @@
 #include <bullet/btBulletCollisionCommon.h>
 
 #include <lua.h>
+//https://lucasklassmann.com/blog/2023-02-26-more-advanced-examples-of-embedding-lua-in-c/
+//https://lucasklassmann.com/blog/2019-02-02-embedding-lua-in-c/
+
+#include <soloud.h>
+#include <soloud_speech.h>
+#include <soloud_thread.h>
 
 int main()
 {
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
+    //IMGUI_CHECKVERSION();
+    //ImGui::CreateContext();
     lp::print("Hello, world!");
-    ImGui::DestroyContext();
+    //ImGui::DestroyContext();
+
+
+    // Define a couple of variables
+    SoLoud::Soloud soloud;  // SoLoud engine core
+    SoLoud::Speech speech;  // A sound source (speech, in this case)
+
+    // Configure sound source
+    speech.setText("1 2 3   1 2 3   Hello world. Welcome to So-Loud.");
+
+    // initialize SoLoud.
+    soloud.init();
+
+    // Play the sound source (we could do this several times if we wanted)
+    soloud.play(speech);
+
+    // Wait until sounds have finished
+    while (soloud.getActiveVoiceCount() > 0)
+    {
+        // Still going, sleep for a bit
+        SoLoud::Thread::sleep(100);
+    }
+
+    // Clean up SoLoud
+    soloud.deinit();
+
+    // All done.
+    return 0;
 }
