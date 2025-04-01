db4de1455ab69edc2a767409b8cbdb821e458c07
ScarletDragonFox <scarletdragonfox@gmail.com>
2025-04-01 14:00:45 +0200

    Create a simple test app for testing sound.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Labyrinth/test_header.hpp>
 
-#include <GLFW/glfw3.h>
+#include <glad/gl.h> //
+#define GLFW_INCLUDE_NONE //we use our own opnegl header <- put this into cmake
+#include <GLFW/glfw3.h> //
+
 #include <assimp/Importer.hpp>
 #include <glm/glm.hpp>
 #include <stb_image.h>
@@ -19,38 +22,213 @@
 #include <soloud.h>
 #include <soloud_speech.h>
 #include <soloud_thread.h>
+#include <soloud_wav.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+
+void glfw_error_callback(int error, const char* description);
 
 int main()
 {
-    //IMGUI_CHECKVERSION();
-    //ImGui::CreateContext();
-    lp::print("Hello, world!");
-    //ImGui::DestroyContext();
+    GLFWwindow* window = nullptr;
+    if(!glfwInit()) return -1;
 
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Define a couple of variables
+    //GLFW_SCALE_TO_MONITOR  GLFW_SCALE_FRAMEBUFFER 
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if(!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    
+    int glversion = gladLoadGL(glfwGetProcAddress);
+    if (glversion == 0) {
+        std::cerr << "Failed to initialize OpenGL context\n";
+        return -1;
+    }
+
+    // Successfully loaded OpenGL
+    std::cout << "Loaded OpenGL " << GLAD_VERSION_MAJOR(glversion) << "." << GLAD_VERSION_MINOR(glversion) << "\n";
+    
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark(); 
+    //ImGui::StyleColorsLight();
+    
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+
     SoLoud::Soloud soloud;  // SoLoud engine core
     SoLoud::Speech speech;  // A sound source (speech, in this case)
 
-    // Configure sound source
-    speech.setText("1 2 3   1 2 3   Hello world. Welcome to So-Loud.");
-
-    // initialize SoLoud.
+    speech.setParams(2000U, 9.0f, 0.5f, 20);
     soloud.init();
+    std::cout << "Using SoLoud backend: " << soloud.getBackendString() << "\n";
+    
+    
+        // SoLoud::Wav sonk; 
+        // if(sonk.load("C:/Users/radek/Music/Cacola - Yellow and Purple.mp3"))
+        // {
+        //     std::cerr << "Could not open file!\n";
+        // }
+        // SoLoud::handle hand =  soloud.play(sonk); //only plays if SoLoud::Wav is still in scope (how, I don't know, but it does!)
+        // soloud.setVolume(hand, 1.0f);
+    
+    bool imguiDoShowDemoWindow = true;
 
-    // Play the sound source (we could do this several times if we wanted)
-    soloud.play(speech);
-
-    // Wait until sounds have finished
-    while (soloud.getActiveVoiceCount() > 0)
+    struct FF
     {
-        // Still going, sleep for a bit
-        SoLoud::Thread::sleep(100);
+        int id = 0;
+        SoLoud::Wav waav;
+        SoLoud::handle handle;
+        std::string window_name = "Default";
+    }; 
+    static int id_cunter = 1;
+    std::vector<FF*> songs_loaded;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (imguiDoShowDemoWindow) ImGui::ShowDemoWindow(&imguiDoShowDemoWindow);
+
+        if(ImGui::Begin("Sound"))
+        {
+            static char speechbuffer[50] = {};
+            static char soundpathbuffer[257] = {};
+            speechbuffer[49] = '\0'; soundpathbuffer[256] = '\0';
+
+            ImGui::InputText("text to say", speechbuffer, 49);
+            if(ImGui::Button("Speak"))
+            {
+                speech.setText(speechbuffer);
+                soloud.play(speech);
+            }
+            ImGui::Separator();
+            
+            ImGui::InputTextWithHint("path to file to play", "sound file path", soundpathbuffer, 256);
+            ImGui::SameLine();
+            if(ImGui::Button("Load & Play"))
+            {
+                std::filesystem::path file = std::filesystem::path(soundpathbuffer);
+                
+                if(std::filesystem::exists(file))
+                {
+                    FF* ssn = new FF; 
+                    ssn->id = id_cunter++;
+                    std::cout << "Trying to load " << file.generic_string().c_str() << "\n";
+                    int resul = ssn->waav.load(file.generic_string().c_str());
+                    if(resul == 0)
+                    {
+                        ssn->handle = soloud.play(ssn->waav);
+                        std::cout << "Playing " << file.generic_string() << " @ " << ssn->handle << "\n";
+                        
+                        ssn->window_name = file.filename().generic_string();
+                        ssn->window_name += " ###" + std::to_string(ssn->id);
+                        songs_loaded.push_back(ssn);
+                        ImGui::OpenPopup(ssn->window_name.c_str());
+                    } else 
+                    {
+                        delete ssn;
+                        ssn = nullptr;
+                        std::cerr << "Sound could not be loaded(" << resul << ")!\n";
+                    }
+                } else std::cerr << "File doesn't exist!\n";
+                
+            }
+            for(auto& item: songs_loaded)
+            {
+                if(ImGui::BeginPopupModal(item->window_name.c_str()))
+                {
+                    if (ImGui::Button("End Song & Close"))
+                    {
+                        soloud.stop(item->handle);
+                        item->waav.stop();
+                        std::cout << "Before delete!\n";
+                        delete(item);
+                        std::cout << "After delete!\n";
+                        ImGui::CloseCurrentPopup();
+                    }
+                    ImGui::EndPopup();
+                }
+            }
+            
+            ImGui::End();
+        }
+       
+        ImGui::Render();
+        
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+
+        glfwSwapBuffers(window);
     }
+    soloud.deinit(); // Clean up SoLoud
 
-    // Clean up SoLoud
-    soloud.deinit();
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
-    // All done.
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
+
+    //TODO:
+    // figure out what audio formats we can work with:
+    // do a window with glfw & glad & imgui to test that out
+    //
+    //  fix bullet3:
+    //      - get the src/ directory from git (manually? or get cmake to do it) 
+    //      - get list of all .c / .cpp files to compile 
+    //      - copy all headers to {build_dir}/include/ (with respect to folders) (find way to automate, maybe with GLOB?)
+    //      - leave notes of what this does and stuff in a deps/bullet3/README.md file <- for maintnance & stuff
+    //
+    //  Merge into main, delete branch
+    //  Create documantation:
+    //      - STYLE.ms <- how to write code so that all code is (relatively) consistant 
+    //      - Read how to make & write a LICENSE.md
+    //      - create new CMakeLists.txt in docs/ to run doxygen wit hright settings & create docs in docs/gen/
+    //      - update main README.md describing every library we are using (see LICENSE.md)
+
+
+    // // Wait until sounds have finished
+    // while (soloud.getActiveVoiceCount() > 0)
+    // {
+    //     // Still going, sleep for a bit
+    //     SoLoud::Thread::sleep(100);
+    // }
+}
+
+
+void glfw_error_callback(int error, const char* description)
+{
+    std::cerr << "ERROR::GLFW " << error << " - " << description << "\n";
 }
